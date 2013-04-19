d12beedf61439f11d73f32abf7c26640bdd3b867
Abhi Hiremagalur and Brian Cunnie <pair+abhi+cunnie@pivotallabs.com>
2013-04-19 14:05:56 -0700

    Deleting old recipes; didn't have git history

--- sprout-osx-apps/recipes/chrome.rb
+++
@@ -1,7 +0,0 @@
-dmg_package "Google Chrome" do
-  dmg_name "googlechrome"
-  source "https://dl-ssl.google.com/chrome/mac/stable/GGRM/googlechrome.dmg"
-  checksum "67de3a3a1d4a130912c5e31c9e2917585b3ec85b927d1498c89f9a360d4e052b"
-  action :install
-  owner WS_USER
-end
