307ef2acf51a3e60f490b3e52cfcd265c67c64dc
Sam Bao <sambao21@gmail.com>
2013-05-05 08:59:42 -0500

    Update rvm version to `stable` * update pivotal_workstation/attributes/versions.rb and set rvm version to stable * add pivotal_workstation/changelog.md * up the pivotal_workstation version in meta data to 1.0.1.pre

---
+++ pivotal_workstation/changelog.md
@@ -0,0 +1,3 @@
+## 1.0.1.pre (unreleased)
+
+ * Update rvm version to `stable`
--- pivotal_workstation/metadata.rb
+++ pivotal_workstation/metadata.rb
@@ -3,7 +3,7 @@ maintainer_email  "accounts@pivotallabs.com"
 license           "MIT"
 description       "Configure frequently-used tools for an OSX workstation"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "1.0.0"
+version           "1.0.1.pre"
 supports          "mac_os_x"
 depends           "dmg"
 depends           "osx"
