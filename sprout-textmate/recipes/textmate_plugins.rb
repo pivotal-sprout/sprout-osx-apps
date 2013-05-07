589e82cf680973952a190066d81ed151058336f4
Brian Cunnie <brian.cunnie@gmail.com>
2013-05-07 10:07:00 -0700

    "unless" doesn't require a "do"

--- sprout-textmate/recipes/textmate_plugins.rb
+++ sprout-textmate/recipes/textmate_plugins.rb
@@ -1,6 +1,6 @@
 homedir = node['etc']['passwd'][node['current_user']]['dir']
 
-unless File.exists?("#{homedir}/Library/Application Support/TextMate/PlugIns/ProjectPlus.tmplugin") do
+unless File.exists?("#{homedir}/Library/Application Support/TextMate/PlugIns/ProjectPlus.tmplugin")
   ["curl -o #{Chef::Config[:file_cache_path]}/projectplus.dmg http://ciaranwal.sh/projectplus-files/ProjectPlus-1.3.dmg",
    "hdiutil mount #{Chef::Config[:file_cache_path]}/projectplus.dmg",
   ].each do |tmate_cmd|
