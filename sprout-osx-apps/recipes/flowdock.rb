e1dcc16133a6b67bd7f0be5a070d25258f41207a
Ludovic Claude <ludovic.claude54@googlemail.com>
2013-05-09 17:31:04 +0200

    Use node['current_user'] instead of WS_USER

--- sprout-osx-apps/recipes/flowdock.rb
+++ sprout-osx-apps/recipes/flowdock.rb
@@ -3,17 +3,17 @@ unless File.exists?("/Applications/Flowdock.app")
     remote_file "#{Chef::Config[:file_cache_path]}/flowdock.zip" do
         source "https://d2ph5hv9wbwvla.cloudfront.net/mac/Flowdock_v1_0_10.zip"
         checksum "fa3d3b05a9a6dd2f359b8ff005baa32b9a99e5139fb19573b3ec2f75e7de496f"
-        owner WS_USER
+        owner node['current_user']
     end
 
     execute "unzip flowdock" do
         command "unzip #{Chef::Config[:file_cache_path]}/flowdock.zip -d #{Chef::Config[:file_cache_path]}/"
-        user WS_USER
+        user node['current_user']
     end
 
     execute "copy flowdock to /Applications" do
         command "mv #{Chef::Config[:file_cache_path]}/Flowdock.app #{Regexp.escape("/Applications/Flowdock.app")}"
-        user WS_USER
+        user node['current_user']
         group "admin"
     end
 
