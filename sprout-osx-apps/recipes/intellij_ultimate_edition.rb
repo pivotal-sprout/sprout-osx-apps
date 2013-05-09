bf29babf8ae5884fbb637dd9dc3cdb3d54f5d87b
Ludovic Claude <ludovic.claude54@googlemail.com>
2013-05-09 17:25:22 +0200

    Update version in IntelliJ name

--- sprout-osx-apps/recipes/intellij_ultimate_edition.rb
+++ sprout-osx-apps/recipes/intellij_ultimate_edition.rb
@@ -1,5 +1,5 @@
 intellij_version = "12.1.2"
-dmg_package "IntelliJ IDEA 10" do
+dmg_package "IntelliJ IDEA 12" do
   source "http://download.jetbrains.com/idea/ideaIU-#{intellij_version}.dmg"
   checksum "75d981489c52e2af8a015186fbd94cb6c72684622c35d4b6ef91f8e929f2ee77"
   owner WS_USER
