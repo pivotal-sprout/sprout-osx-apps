4bd50d39510bd0c631fb579dc44401c1e38c6baa
Brian Cunnie <pair+cunnie@pivotallabs.com>
2013-05-10 15:37:02 -0700

    bumped LibreOffice to 4.0.3

--- sprout-osx-apps/recipes/libreoffice.rb
+++ sprout-osx-apps/recipes/libreoffice.rb
@@ -1,7 +1,7 @@
 dmg_package "LibreOffice" do
   volumes_dir "LibreOffice"
-  source "http://download.documentfoundation.org/libreoffice/stable/4.0.2/mac/x86/LibreOffice_4.0.2_MacOS_x86.dmg"
-  checksum "98a8a7a6aed21d173bf0e8b06d0ff30dcd807550837e41e72670affa8bdd71b6"
+  source "http://download.documentfoundation.org/libreoffice/stable/4.0.3/mac/x86/LibreOffice_4.0.3_MacOS_x86.dmg"
+  checksum "11eab948d4982340ef92ec46d3bb247c087cf7d831bd1c0c6ffcfb3b6bfd28d2"
   owner node['current_user']
   action :install
 end
