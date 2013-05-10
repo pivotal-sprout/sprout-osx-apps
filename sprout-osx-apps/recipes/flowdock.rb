7049c8f93056481f8aa9b34b30e015fa45214fa0
Ludovic Claude <ludovic.claude54@googlemail.com>
2013-05-10 23:53:02 +0200

    Parameterize Flowdock

--- sprout-osx-apps/recipes/flowdock.rb
+++ sprout-osx-apps/recipes/flowdock.rb
@@ -1,8 +1,8 @@
-unless File.exists?("/Applications/Flowdock.app")
+unless File.exists?(node["flowdock_app_path"])
 
     remote_file "#{Chef::Config[:file_cache_path]}/flowdock.zip" do
-        source "https://d2ph5hv9wbwvla.cloudfront.net/mac/Flowdock_v1_0_10.zip"
-        checksum "fa3d3b05a9a6dd2f359b8ff005baa32b9a99e5139fb19573b3ec2f75e7de496f"
+        source node["flowdock_download_uri"]
+        checksum node["flowdock_sha"]
         owner node['current_user']
     end
 
@@ -12,14 +12,14 @@ unless File.exists?("/Applications/Flowdock.app")
     end
 
     execute "copy flowdock to /Applications" do
-        command "mv #{Chef::Config[:file_cache_path]}/Flowdock.app #{Regexp.escape("/Applications/Flowdock.app")}"
+        command "mv #{Chef::Config[:file_cache_path]}/Flowdock.app #{Regexp.escape(node["flowdock_app_path"])}"
         user node['current_user']
         group "admin"
     end
 
     ruby_block "test to see if Flowdock.app was installed" do
         block do
-            raise "Flowdock.app was not installed" unless File.exists?("/Applications/Flowdock.app")
+            raise "Flowdock.app was not installed" unless File.exists?(node["flowdock_app_path"])
         end
     end
 
