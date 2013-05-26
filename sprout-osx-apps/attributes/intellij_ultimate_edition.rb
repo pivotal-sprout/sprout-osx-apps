06a8c2cc9336da3fe720421ab2ef3b320ac240c0
Ludovic Claude <ludovic.claude54@googlemail.com>
2013-05-26 14:37:09 +0200

    Fix attribute name for Intellij Ultimate

--- sprout-osx-apps/attributes/intellij_ultimate_edition.rb
+++ sprout-osx-apps/attributes/intellij_ultimate_edition.rb
@@ -1,3 +1,3 @@
-node.default["intellij_community_edition_download_uri"]="http://download.jetbrains.com/idea/ideaIU-12.1.2.dmg"
-node.default["intellij_community_edition_sha"]="75d981489c52e2af8a015186fbd94cb6c72684622c35d4b6ef91f8e929f2ee77"
-node.default["intellij_community_edition_package_name"]="IntelliJ IDEA 12"
+node.default["intellij_ultimate_edition_download_uri"]="http://download.jetbrains.com/idea/ideaIU-12.1.2.dmg"
+node.default["intellij_ultimate_edition_sha"]="75d981489c52e2af8a015186fbd94cb6c72684622c35d4b6ef91f8e929f2ee77"
+node.default["intellij_ultimate_edition_package_name"]="IntelliJ IDEA 12"
