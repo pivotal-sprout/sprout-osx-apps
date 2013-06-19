00cc176d5fc710ea08ab5d1134ea7e680fa284e7
Brian Cunnie <cunnie@pivotallabs.com>
2013-06-18 19:46:27 -0700

    bash_it version in right place
    Signed-off-by: Abhijit Hiremagalur <abhi@pivotallabs.com>

--- pivotal_workstation/attributes/versions.rb
+++ pivotal_workstation/attributes/versions.rb
@@ -1,4 +1,1 @@
-node.default['versions'] = {
-  "bash_it" => "5cb0ecc1c813bc5619e0f708b8015a4596a37d6c",
-  "rvm" => "d2c2e4d453f7b9723e63ebee3b0ea037f0c6bbad"
-}
+node.default['versions']['rvm'] = 'd2c2e4d453f7b9723e63ebee3b0ea037f0c6bbad'
---
+++ sprout-osx-base/attributes/versions.rb
@@ -0,0 +1,1 @@
+node.default['versions']['bash_it'] = '5cb0ecc1c813bc5619e0f708b8015a4596a37d6c'
