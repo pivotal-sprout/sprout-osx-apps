c52d93ea84360961a436ab54f6a3c462049d0ac4
Cliff Erson <cerson@me.com>
2013-06-21 16:42:06 -0700

    Depreciate pivotal_workstation::java for sprout-osx-apps:java

--- pivotal_workstation/recipes/java.rb
+++ pivotal_workstation/recipes/java.rb
@@ -1,10 +1,3 @@
-return unless node["platform"] == "mac_os_x"
+Chef::Log.warn("[pivotal_workstation::java] is deprecated please use [sprout-osx-apps::java] in the future.")
 
-dmg_package "JavaForOSX" do
-  source "http://support.apple.com/downloads/DL1572/en_US/"
-  volumes_dir "Java for OS X 2013-003"
-  action :install
-  type "pkg"
-  package_id "com.apple.pkg.JavaForMacOSX107"
-  checksum "193daf9d16ba753cb7b9160e3b3842e515575a07ac2ec06d27c21a40c30c917b"
-end
+include_recipe "sprout-osx-apps::java"
