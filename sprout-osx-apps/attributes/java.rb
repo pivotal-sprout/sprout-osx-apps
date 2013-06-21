4c5ee9df32d0bf0293e47bd1062126d15ddb6c73
Cliff Erson <cerson@me.com>
2013-06-21 16:41:48 -0700

    Move Java's dmg_package source, volumes_dir & checksum to attributes file

---
+++ sprout-osx-apps/attributes/java.rb
@@ -0,0 +1,3 @@
+default['pivotal_workstation']['java']['dmg_package']['source'] = "http://support.apple.com/downloads/DL1572/en_US/"
+default['pivotal_workstation']['java']['dmg_package']['volumes_dir'] = "Java for OS X 2013-004"
+default['pivotal_workstation']['java']['dmg_package']['checksum'] = "29cba3086658af9e423453a6fb0e90109e0871b20343936b1c18bd356a67530e"
