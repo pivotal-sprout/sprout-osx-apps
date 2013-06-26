93f6e227af7d000393e4f7198773f5bc7579fd8e
Brian Cunnie and Jonathan Barnes <pair+cunnie+jonathan@pivotallabs.com>
2013-06-25 20:35:59 -0700

    Updates Terminal preferences to start with the Pro profile
    Attempts to change the font to Menlo 18pt but mostly doesn't stick.

--- sprout-osx-settings/recipes/terminal_preferences.rb
+++ sprout-osx-settings/recipes/terminal_preferences.rb
@@ -1,11 +1,25 @@
-osx_defaults "Set terminal color scheme to #{node['terminal']['color_scheme']}" do
+osx_defaults "Set terminal color scheme to #{node['terminal']['profile']}" do
   domain 'com.apple.Terminal'
   key 'Default Window Settings'
-  string node['terminal']['color_scheme']
+  string node['terminal']['profile']
 end
 
-osx_defaults "Set startup terminal color scheme to #{node['terminal']['color_scheme']}" do
+osx_defaults "Set startup terminal color scheme to #{node['terminal']['profile']}" do
   domain 'com.apple.Terminal'
   key 'Startup Window Settings'
-  string node['terminal']['color_scheme']
+  string node['terminal']['profile']
 end
+
+execute "Updates font to Menlo-Regular 18pt" do
+  command "/usr/libexec/PlistBuddy -c 'import :\"Window Settings\":Pro:Font /tmp/MenloRegular18pt.bin' #{ENV['HOME']}/Library/Preferences/com.apple.Terminal.plist"
+  user node['current_user']
+end
+
+ruby_block "let's see what it looks like" do
+  block do
+    puts `/usr/libexec/PlistBuddy -c 'print :"Window Settings":Pro:Font ' #{ENV['HOME']}/Library/Preferences/com.apple.Terminal.plist | strings`
+  end
+end
+
+
+execute 'killall Finder; killall Dock; killall SystemUIServer'
