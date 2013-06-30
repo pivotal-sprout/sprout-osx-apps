35333d598d8d823cb6fcaca9f04ad24966b94b27
Cliff Erson <cerson@me.com>
2013-06-29 21:47:11 -0700

    Add 'addloginitem' execute resource to SizeUp

--- pivotal_workstation/recipes/sizeup.rb
+++ pivotal_workstation/recipes/sizeup.rb
@@ -1,3 +1,5 @@
+include_recipe "pivotal_workstation::addloginitem"
+
 unless File.exists?("/Applications/SizeUp.app")
   include_recipe "pivotal_workstation::enable_assistive_devices"
 
@@ -6,15 +8,20 @@ unless File.exists?("/Applications/SizeUp.app")
     mode "0644"
   end
 
-  # start up on login
-  execute "Start SizeUp automatically" do
-    command "defaults write loginwindow AutoLaunchedApplicationDictionary -array-add '{ \"Path\" = \"/Applications/SizeUp.app\"; \"Hide\" = 0; }'"
-    user node['current_user']
-  end
-
   execute "unzip sizeup" do
     command "unzip #{Chef::Config[:file_cache_path]}/sizeup.zip SizeUp.app/* -d /Applications/"
     user node['current_user']
     group "admin"
+    notifies :run, "execute[Start SizeUp on login]"
   end
+
+  app_path = "/Applications/SizeUp.app"
+
+  # start up on login
+  execute "Start SizeUp on login" do
+    command "addloginitem #{app_path}"
+    user node['current_user']
+    action :nothing
+  end
+
 end
