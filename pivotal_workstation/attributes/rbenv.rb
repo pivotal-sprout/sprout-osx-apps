a289259cd28b4ed77291bb910db0de5a4c0e96a2
Abhi Hiremagalur <abhijit@hiremaga.com>
2013-07-06 17:52:20 -0700

    Update rbenv rubies to latest

--- pivotal_workstation/attributes/rbenv.rb
+++ pivotal_workstation/attributes/rbenv.rb
@@ -1,11 +1,9 @@
 unless node["rbenv"]
-  node.default["rbenv"]= {
+  node.default["rbenv"] = {
     "rubies" => {
-        "ree-1.8.7-2012.01" => {},
-        "1.8.7-p352" => {},
-        "1.9.2-p290" => {},
-        "1.9.3-p125" => { :env => {"CC" => "clang"}}
+        "1.9.3-p448" => {},
+        "2.0.0-p247" => {}
       },
-    "default_ruby" => "1.9.3-p125"
+    "default_ruby" => "1.9.3-p448"
   }
 end
