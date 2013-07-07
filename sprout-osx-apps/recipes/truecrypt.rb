97048a5b65aff1dd4f586cb8269163439400a212
James Cuzella <james.cuzella@lyraphase.com>
2013-07-07 14:50:20 -0600

    Added main truecrypt package_id so install action only runs the first time

--- sprout-osx-apps/recipes/truecrypt.rb
+++ sprout-osx-apps/recipes/truecrypt.rb
@@ -3,6 +3,7 @@ dmg_package "TrueCrypt 7.1a" do
   checksum '04db58b737c05bb6b0b83f1cb37a29edec844b59ff223b9e213ee1f4e287f586'
   type 'mpkg'
   accept_eula true
+  package_id 'org.TrueCryptFoundation.TrueCrypt' # mpkg also installs: com.github.osxfuse.pkg.Core, com.google.macfuse.core
   action :install
   owner node['current_user']
 end
