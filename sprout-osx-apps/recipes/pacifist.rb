4f16d01b351d779170777e5233848770dbdb2e99
James Cuzella <james.cuzella@lyraphase.com>
2013-07-09 23:36:40 -0600

    Adding Pacifist app

---
+++ sprout-osx-apps/recipes/pacifist.rb
@@ -0,0 +1,9 @@
+dmg_properties = node['sprout']['pacifist']['dmg']
+
+dmg_package "Pacifist" do
+  source   dmg_properties['source']
+  checksum dmg_properties['checksum']
+  dmg_name dmg_properties['dmg_name']
+  action :install
+  owner node['current_user']
+end
