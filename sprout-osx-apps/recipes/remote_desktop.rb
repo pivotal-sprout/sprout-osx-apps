0e9bd9be52d083ddc28554c6b75d32be3d6715ab
Brian Cunnie <cunnie@pivotallabs.com>
2013-07-11 12:09:13 -0700

    Microsoft Remote Desktop 2.1.1

---
+++ sprout-osx-apps/recipes/remote_desktop.rb
@@ -0,0 +1,6 @@
+dmg_package "RDC Installer" do
+  source "http://download.microsoft.com/download/C/F/0/CF0AE39A-3307-4D39-9D50-58E699C91B2F/RDC_2.1.1_ALL.dmg"
+  volumes_dir "RDC"
+  checksum "4ebe551c9ee0e2da6b8f746be13c2df342c6f14cd3fbedbf2ab490f09b44616f"
+  type "mpkg"
+end
