ad6e8f7bb6f7eeebe662a318ddfdb072bb3ddcd7
James Cuzella <james.cuzella@lyraphase.com>
2013-07-16 14:23:49 -0600

    Updating GPGTools to GPG Suite 2013.07.27
     - Fix ownership perms of user's Mail dir after install

--- sprout-osx-apps/recipes/gpgtools.rb
+++ sprout-osx-apps/recipes/gpgtools.rb
@@ -1,9 +1,23 @@
-dmg_properties = node['sprout']['gpgtools']['dmg']
+dmg_properties = node['sprout']['gpgsuite']['dmg']
 
-dmg_package "GPGTools" do
+dmg_package "GPG Suite" do
   source      dmg_properties['source']
   checksum    dmg_properties['checksum']
+  volumes_dir dmg_properties['volumes_dir']
+  dmg_name    dmg_properties['dmg_name']
+  app         dmg_properties['app']
   type        dmg_properties['type']
   package_id 'org.gpgtools.*'
+  owner node['current_user']
+end
+
+# Ensure user's mail folder permissions are correct (after install, owned by root)
+directory "/Users/#{node['current_user']}/Library/Mail" do
+  mode 0700
+  owner node['current_user']
+end
+
+directory "/Users/#{node['current_user']}/.gnupg" do
+  mode 0700
   owner node['current_user']
 end
