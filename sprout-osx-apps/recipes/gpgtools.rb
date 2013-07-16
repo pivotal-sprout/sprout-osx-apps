be7689772ec2b4ec32d658027d327d882dc25e11
James Cuzella <james.cuzella@lyraphase.com>
2013-07-16 14:15:34 -0600

    Move GPGTools' dmg_package source, checksum & type to attributes file
        Notes:
    
          To use a custom version, put something like this in your
          soloistrc:
    
            node_attributes:
              sprout:
                gpgtools:
                  dmg:
                    source: https://s3.amazonaws.com/gpgtools/GPG%20Suite%20-%202013.07.27.dmg
                    checksum: eab2a899578b4138babfd816054878ddcd867fac3b938b425174cab5c08d1b66
                    type: pkg

--- sprout-osx-apps/recipes/gpgtools.rb
+++ sprout-osx-apps/recipes/gpgtools.rb
@@ -1,8 +1,9 @@
+dmg_properties = node['sprout']['gpgtools']['dmg']
+
 dmg_package "GPGTools" do
-  source 'https://s3.amazonaws.com/gpgtools/GPGTools-2013.5.20.dmg'
-  checksum '032780e5fc4712410acd8d122aa833134ab4c87eb37b5c735e9f623f0720d387'
-  type 'pkg'
+  source      dmg_properties['source']
+  checksum    dmg_properties['checksum']
+  type        dmg_properties['type']
   package_id 'org.gpgtools.*'
-  action :install
   owner node['current_user']
 end
