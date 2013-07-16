75a916b7c83dd792bbfdaeeb24c8573b0c58197d
James Cuzella <james.cuzella@lyraphase.com>
2013-07-16 14:15:34 -0600

    Move Adium's dmg_package source, checksum & volumes_dir to attributes file
        Notes:
    
          By default, the recipe will use version 1.5.6 of Adium.
          To use a custom version, put something like this in your
          soloistrc:
    
            node_attributes:
              sprout:
                adium:
                  dmg:
                    source: http://sourceforge.net/projects/adium/files/Adium_1.5.7.dmg/download
                    checksum: f80408a2c5bc97be8adb35ffeb7c4501392e847edae4e6e24d87da72de7e96b9
                    volumes_dir: Adium 1.5.7

---
+++ sprout-osx-apps/attributes/adium.rb
@@ -0,0 +1,3 @@
+default['sprout']['adium']['dmg']['source']      = 'http://sourceforge.net/projects/adium/files/Adium_1.5.6.dmg/download'
+default['sprout']['adium']['dmg']['checksum']    = 'd5f580b7db57348c31f8e0f18691d7758a65ad61471bf984955360f91b21edb8'
+default['sprout']['adium']['dmg']['volumes_dir'] = 'Adium 1.5.6'
