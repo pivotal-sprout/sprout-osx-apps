16051587a65bd16da862dc2b854d8af1aebc89f3
James Cuzella <james.cuzella@lyraphase.com>
2013-07-16 14:23:49 -0600

    Updating default chrome installed to 28.0.1500.71

--- sprout-osx-apps/attributes/chrome.rb
+++ sprout-osx-apps/attributes/chrome.rb
@@ -1,3 +1,3 @@
-default['sprout']['chrome']['dmg']['source']      = 'https://dl-ssl.google.com/chrome/mac/stable/GGRM/googlechrome.dmg'
-default['sprout']['chrome']['dmg']['checksum']    = '67de3a3a1d4a130912c5e31c9e2917585b3ec85b927d1498c89f9a360d4e052b'
+default['sprout']['chrome']['dmg']['source']      = 'https://dl.google.com/chrome/mac/stable/CHFA/googlechrome.dmg'
+default['sprout']['chrome']['dmg']['checksum']    = '5cb154403f1e638b9bcca68c0c80dc2b562e9d3a14c7db3e370428dcd451891a'
 default['sprout']['chrome']['dmg']['dmg_name']    = 'googlechrome'
