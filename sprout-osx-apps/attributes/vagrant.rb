664eace6f71b54091f8690b1892e675f82761a71
Abhi Hiremagalur <abhijit@hiremaga.com>
2013-07-17 20:11:00 -0700

    Upgrade vagrant to 1.2.4

--- sprout-osx-apps/attributes/vagrant.rb
+++ sprout-osx-apps/attributes/vagrant.rb
@@ -1,2 +1,2 @@
-default['sprout']['vagrant']['dmg']['source']      = 'http://files.vagrantup.com/packages/7e400d00a3c5a0fdf2809c8b5001a035415a607b/Vagrant-1.2.2.dmg'
-default['sprout']['vagrant']['dmg']['checksum']    = '1581552841e076043308f330a5b1130b455c604846116c54b5330bb17240c7ee'
+default['sprout']['vagrant']['dmg']['source']      = 'http://files.vagrantup.com/packages/0219bb87725aac28a97c0e924c310cc97831fd9d/Vagrant-1.2.4.dmg'
+default['sprout']['vagrant']['dmg']['checksum']    = '6b84406945c298cc014ec8bc910c6716c9f405640128917ff8992b325f210421'
