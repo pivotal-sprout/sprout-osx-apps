dbeeb483c7362a238b97cc8767a7941d60180b1f
Cliff Erson <cerson@me.com>
2013-07-21 11:21:48 -0700

    Add 'addloginitem' execute resource to Dropbox

--- sprout-osx-apps/recipes/dropbox.rb
+++ sprout-osx-apps/recipes/dropbox.rb
@@ -1,7 +1,18 @@
+include_recipe "sprout-osx-base::addloginitem"
+
 dmg_package "Dropbox" do
   volumes_dir "Dropbox Installer"
   source "https://www.dropbox.com/download?plat=mac"
   checksum "1169d604ed3db8b15449753f7cf9c5af2b86afefcf598231d44dc29551c4a489"
   action :install
   owner node['current_user']
+  notifies :run, "execute[Start Dropbox on login]"
+end
+
+app_path ="/Applications/Dropbox.app"
+
+execute "Start Dropbox on login" do
+  command "addloginitem #{app_path}"
+  user node['current_user']
+  action :nothing
 end
