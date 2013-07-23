95b9a0767916600a39716ac9152de58464e1c550
James Cuzella <james.cuzella@lyraphase.com>
2013-07-23 16:42:32 -0600

    Making sprout-osx-settings::remove_expose_keyboard_shortcuts idempotent for faster chef run

--- sprout-osx-settings/recipes/remove_expose_keyboard_shortcuts.rb
+++ sprout-osx-settings/recipes/remove_expose_keyboard_shortcuts.rb
@@ -1,41 +1,43 @@
 include_recipe "sprout-osx-settings::enable_assistive_devices"
 
-ruby_block "Remove Expose Keyboard Shortcuts" do
+["all windows", "application windows", "show desktop", "dashboard" ].each do |shortcut_name|
+ruby_block "Remove Expose Keyboard Shortcut for '#{shortcut_name}'" do
   block do
     system(
     "osascript -e '
-      tell application \"System Preferences\"
-      	set current pane to pane \"com.apple.preference.expose\"
-      end tell
-
       tell application \"System Events\"
-      	tell application process \"System Preferences\"
-      	  click pop up button 1 of group 1 of window \"Mission Control\"
-      	  delay 1
-      		keystroke \"-\"
-      		delay 1
-      		keystroke return
-
-  			  click pop up button 2 of group 1 of window \"Mission Control\"
-      	  delay 1
-      		keystroke \"-\"
-      		delay 1
-      		keystroke return
-
-          click pop up button 3 of group 1 of window \"Mission Control\"
-          delay 1
-          keystroke \"-\"
-          delay 1
-          keystroke return
-
-          click pop up button 4 of group 1 of window \"Mission Control\"
-          delay 1
-          keystroke \"-\"
-          delay 1
-          keystroke return
-      	end tell
-      	quit application \"System Preferences\"
+        if UI elements enabled then
+          tell expose preferences
+            set the properties of the #{shortcut_name} shortcut to {function key:none, function key modifiers:{none}}
+            set the properties of the #{shortcut_name} shortcut to {function key:none}
+          end tell
+        else
+          tell application \"System Preferences\"
+            activate
+            set current pane to pane \"com.apple.preference.universalaccess\"
+            display dialog \"UI element scripting is not enabled. Check \\\"Enable access for assistive devices\\\"\"
+          end tell
+        end if
       end tell'"
+    )
+  end
+  not_if { system("osascript -e '
+              tell application \"System Events\"
+                tell expose preferences
+                  get the properties of the #{shortcut_name} shortcut
+                end tell
+              end tell' | grep -q 'function key:none, function key modifiers:,'")
+          }
+  end
+end
+
+ruby_block "Quit System Preferences" do
+  block do
+    system(
+    "osascript -e '
+      if application \"System Preferences\" is running then
+        tell application \"System Preferences\" to quit
+      end if'"
     )
   end
 end
