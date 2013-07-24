bac06e74e1b6c0a300eaa6398494c47d3626a924
James Cuzella <james.cuzella@lyraphase.com>
2013-07-24 11:14:10 -0600

    Adding Hiss

---
+++ sprout-osx-apps/attributes/hiss.rb
@@ -0,0 +1,4 @@
+default['sprout']['hiss']['zip']['source'] = 'http://collect3.com.au/hiss/Hiss.zip'
+default['sprout']['hiss']['zip']['checksum'] = 'c6a1181602e481068d8eeda05d26bee5ac8435ee752da63786eaaa742df16580'
+default['sprout']['hiss']['zip']['app_bundle_name'] = 'Hiss.app'
+default['sprout']['hiss']['install_dir'] = '/Applications'
