204bdfe5a81bbd7f9c6fec0b1dd2d613d0b7620b
James Cuzella <james.cuzella@lyraphase.com>
2013-07-27 22:09:39 -0600

    Force upgrade to stable GPG Suite 2013.07.27

--- sprout-osx-apps/recipes/gpgtools.rb
+++ sprout-osx-apps/recipes/gpgtools.rb
@@ -7,7 +7,8 @@ dmg_package "GPG Suite" do
   dmg_name    dmg_properties['dmg_name']
   app         dmg_properties['app']
   type        dmg_properties['type']
-  package_id 'org.gpgtools.*'
+  package_id 'org.gpgtools.libmacgpgB.pkg' # Force upgrade from old GPGTools since dmg_package has no action :upgrade
+  # package_id 'org.gpgtools.*'
   owner node['current_user']
 end
 
