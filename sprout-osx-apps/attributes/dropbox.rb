2a579fe60a38559bbfb418b3a3596e766cf090ef
Mike Fiedler <miketheman@gmail.com>
2013-07-29 09:42:00 -0400

    moves dropbox install details to attributes

---
+++ sprout-osx-apps/attributes/dropbox.rb
@@ -0,0 +1,2 @@
+default['sprout']['dropbox']['dmg']['source'] = 'https://www.dropbox.com/download?plat=mac'
+default['sprout']['dropbox']['dmg']['checksum'] = '1169d604ed3db8b15449753f7cf9c5af2b86afefcf598231d44dc29551c4a489'
