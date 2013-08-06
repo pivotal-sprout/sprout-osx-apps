5e66eb8a91334883ec51ac570dd5cd35a5cbc01d
James Cuzella <james.cuzella@lyraphase.com>
2013-08-06 15:34:06 -0600

    Fix FC002, FC023 in daisydisk recipe

--- sprout-osx-apps/recipes/daisydisk.rb
+++ sprout-osx-apps/recipes/daisydisk.rb
@@ -13,12 +13,9 @@ recursive_directories([app_supportdir, "DaisyDisk"]) do
   owner node['current_user']
 end
 
-unless node['sprout']['daisydisk']['license']['customer_name'].nil? || node['sprout']['daisydisk']['license']['registration_key'].nil?
-  template "#{File.join app_supportdir, 'DaisyDisk', 'License.DaisyDisk'}" do
-    source "License.DaisyDisk.erb"
-    owner node['current_user']
-    variables :license => node['sprout']['daisydisk']['license']
-    # action :create_if_missing
-    action :create
-  end
+template File.join(app_supportdir, 'DaisyDisk', 'License.DaisyDisk') do
+  source "License.DaisyDisk.erb"
+  owner node['current_user']
+  variables :license => node['sprout']['daisydisk']['license']
+  not_if { node['sprout']['daisydisk']['license']['customer_name'].nil? || node['sprout']['daisydisk']['license']['registration_key'].nil? }
 end
