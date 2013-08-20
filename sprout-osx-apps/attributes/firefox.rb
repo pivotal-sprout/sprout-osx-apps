6395cff35895e2c8cef41c685a4637cb91cec244
Chris Boone <cboone@pivotallabs.com>
2013-08-20 15:12:53 -0700

    Extract Firefox Aurora attributes

---
+++ sprout-osx-apps/attributes/firefox.rb
@@ -0,0 +1,3 @@
+default['sprout']['firefox']['aurora']['dmg']['app']         = 'FirefoxAurora'
+default['sprout']['firefox']['aurora']['dmg']['source']      = 'https://ftp.mozilla.org/pub/mozilla.org/firefox/nightly/latest-mozilla-aurora/firefox-24.0a2.en-US.mac.dmg'
+default['sprout']['firefox']['aurora']['dmg']['volumes_dir'] = 'Aurora'
--- sprout-osx-apps/recipes/firefox_aurora.rb
+++ sprout-osx-apps/recipes/firefox_aurora.rb
@@ -1,9 +1,11 @@
-#Installs Firefox Aurora (Pre Beta Build of Firefox)
-#http://www.mozilla.org/en-US/firefox/aurora/
-dmg_package "Firefox Aurora" do
-  volumes_dir "Aurora"
-  app "FirefoxAurora"
-  source "https://ftp.mozilla.org/pub/mozilla.org/firefox/nightly/latest-mozilla-aurora/firefox-24.0a2.en-US.mac.dmg"
+# Installs Firefox Aurora (pre-beta build of Firefox)
+# http://www.mozilla.org/en-US/firefox/aurora/
+
+dmg_package 'Firefox Aurora' do
+  config = node['sprout']['firefox']['aurora']['dmg']
+  volumes_dir config['volumes_dir']
+  app config['app']
+  source config['source']
   action :install
   owner node['current_user']
 end
