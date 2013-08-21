23f1368c6742af877e9572bcbac5dad7b2ef9cea
Chris Boone <cboone@pivotallabs.com>
2013-08-21 12:51:22 -0700

    Add TableTennisTime installation recipe

---
+++ sprout-osx-apps/attributes/table_tennis_time.rb
@@ -0,0 +1,3 @@
+default['sprout']['table_tennis_time']['application_name'] = 'TableTennisTime.app'
+default['sprout']['table_tennis_time']['zip']['checksum'] = '2d829f51d59671d290ff83f625c66be0edbbb1821e51304e592bd79cd2d041a0'
+default['sprout']['table_tennis_time']['zip']['url'] = 'http://tabletennistime.cfapps.io/public/TableTennisTime.app.zip'
