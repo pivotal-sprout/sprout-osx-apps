366d51e3d51dee02033d061229a6d0be27a11aac
Abhi Hiremagalur <abhijit@hiremaga.com>
2013-08-24 10:46:55 -0700

    Update Java dmg

--- sprout-osx-apps/attributes/java.rb
+++ sprout-osx-apps/attributes/java.rb
@@ -1,3 +1,3 @@
-default['sprout']['java']['dmg']['source']      = "http://support.apple.com/downloads/DL1572/en_US/"
+default['sprout']['java']['dmg']['source']      = "http://support.apple.com/downloads/DL1572/en_US/JavaForOSX2013-004.dmg"
 default['sprout']['java']['dmg']['volumes_dir'] = "Java for OS X 2013-004"
-default['sprout']['java']['dmg']['checksum']    = "29cba3086658af9e423453a6fb0e90109e0871b20343936b1c18bd356a67530e"
+default['sprout']['java']['dmg']['checksum']    = "b10cac904a28be56b898563756caa99c2d484da605d2f7aa12547a4850b4fda5"
