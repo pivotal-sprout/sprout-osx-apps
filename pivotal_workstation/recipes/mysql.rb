e7804af136ea5e5153c3136820a48e940149ec6e
Brian Cunnie <cunnie@pivotallabs.com>
2013-09-10 12:26:10 -0700

    D'oh!  I should have deleted BOTH broken stanzas

--- pivotal_workstation/recipes/mysql.rb
+++ pivotal_workstation/recipes/mysql.rb
@@ -17,18 +17,13 @@ include_recipe "sprout-osx-base::homebrew"
   end
 end
 
+# Delete the 'perl -pi -e' stanzas as soon as Mavericks/Homebrew/MySQL is fixed
 # https://github.com/mxcl/homebrew/issues/21367
 execute 'perl -pi -e "s~<string>10.9</string>~<string>10.8</string>~" /System/Library/CoreServices/SystemVersion.plist'
 
 brew "mysql"
 
 execute 'perl -pi -e "s~<string>10.8</string>~<string>10.9</string>~" /System/Library/CoreServices/SystemVersion.plist'
-
-osx_defaults "I'm done with pretending!" do
-  domain '/System/Library/CoreServices/SystemVersion.plist'
-  key 'ProductVersion'
-  string '10.8'
-end
 
 ruby_block "copy mysql plist to ~/Library/LaunchAgents" do
   block do
