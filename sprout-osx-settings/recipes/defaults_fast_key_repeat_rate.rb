9cec50aa81278e1211bbbb291af32919869a8085
Brian Cunnie <cunnie@pivotallabs.com>
2013-09-10 18:44:27 -0700

    Makes key repeat and initial delay rates configurable
    Also increases the configured keyboard repeat rate value to 1
    
    This is an aggressive repeat rate suitable for
    professional programmers (and if you don't accept
    this pull request we're taking over this repo!)
    
    Signed-off-by: Jonathan Barnes <jonathan@codeword.net>

--- sprout-osx-settings/recipes/defaults_fast_key_repeat_rate.rb
+++ sprout-osx-settings/recipes/defaults_fast_key_repeat_rate.rb
@@ -1,17 +1,13 @@
-# Requires logout to take effect
-
-key_repeat_rate_lower_is_faster = 2
-
 osx_defaults "set key repeat rate" do
   domain "/Users/#{node['current_user']}/Library/Preferences/.GlobalPreferences"
   key "KeyRepeat"
-  integer key_repeat_rate_lower_is_faster
+  integer node['keyboard_preferences']['key_repeat_rate_lower_is_faster']
+  only_if { node['keyboard_preferences']['key_repeat_rate_lower_is_faster'] }
 end
 
-initial_repeat_delay_lower_is_shorter = 15
-
-osx_defaults "set key repeat rate" do
+osx_defaults "set initial key repeat delay" do
   domain "/Users/#{node['current_user']}/Library/Preferences/.GlobalPreferences"
   key "InitialKeyRepeat"
-  integer initial_repeat_delay_lower_is_shorter
+  integer node['keyboard_preferences']['initial_repeat_delay_lower_is_shorter']
+  only_if { node['keyboard_preferences']['initial_repeat_delay_lower_is_shorter'] }
 end
