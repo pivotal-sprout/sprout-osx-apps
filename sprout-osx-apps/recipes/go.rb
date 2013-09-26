21c39ee2a7289813062d624f05f2d1ab17f0adb1
Abhi Hiremagalur <abhi@pivotallabs.com>
2013-09-25 21:56:48 -0700

    Add cross-compilers and runtime support
    Most of the go projects on CF use this

--- sprout-osx-apps/recipes/go.rb
+++ sprout-osx-apps/recipes/go.rb
@@ -2,7 +2,9 @@ include_recipe 'sprout-osx-base::bash_it'
 include_recipe 'sprout-osx-apps::hg'
 include_recipe 'sprout-osx-apps::bzr'
 
-brew 'go'
+brew 'go' do
+  options '--cross-compile-all'
+end
 
 sprout_osx_base_bash_it_custom_plugin 'bash_it/custom/go.bash' do
   cookbook 'sprout-osx-apps'
