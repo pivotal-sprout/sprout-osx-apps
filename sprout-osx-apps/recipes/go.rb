a66d0f0868377b707da621d08b9950f665498d51
Abhi Hiremagalur <abhi@pivotallabs.com>
2013-09-25 19:40:26 -0700

    Extend recipe for go to setup typical dependencies
    * GOPATH per http://golang.org/doc/code.html#GOPATH
    * Mercurial (hg) for go get
    * Bazaar (bzr) for go get
    * Modify PYTHONPATH for hg & bzr to work correctly on OSX

--- sprout-osx-apps/recipes/go.rb
+++ sprout-osx-apps/recipes/go.rb
@@ -1,1 +1,9 @@
-brew "go"
+include_recipe 'sprout-osx-base::bash_it'
+include_recipe 'sprout-osx-apps::hg'
+include_recipe 'sprout-osx-apps::bzr'
+
+brew 'go'
+
+sprout_osx_base_bash_it_custom_plugin 'bash_it/custom/go.bash' do
+  cookbook 'sprout-osx-apps'
+end
---
+++ sprout-osx-apps/templates/default/bash_it/custom/python_path.bash
@@ -0,0 +1,1 @@
+export PYTHONPATH=/usr/local/lib/python2.7/site-packages:$PYTHONPATH
