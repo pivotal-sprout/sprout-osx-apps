a709f31c2ca9fae9eb07163485e11ebbe1b6976e
Aram Price <aprice@pivotallabs.com>
2013-10-15 18:07:34 -0700

    Fix hipchat recipe when never started
    Signed-off-by: Abhijit Hiremagalur <abhi@pivotallabs.com>

--- sprout-osx-apps/attributes/hipchat.rb
+++ sprout-osx-apps/attributes/hipchat.rb
@@ -1,5 +1,5 @@
 default['sprout']['hipchat']['url'] = "http://downloads.hipchat.com.s3.amazonaws.com/osx/HipChat-2.0.zip"
 default['sprout']['hipchat']['app'] = "HipChat.app"
 default['sprout']['hipchat']['settings'] = [
-  ['disableSounds', 'true']
+  ['disableSounds', 'bool', 'true']
 ]
