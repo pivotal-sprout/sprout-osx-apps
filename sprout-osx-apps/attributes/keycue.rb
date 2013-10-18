e4fec202bb973ce291c222a7bf5fa2c07ae4326e
Marcos Sainz <marcos+gldr-msainz-desktop@glassdoor.com>
2013-10-18 15:14:32 -0700

    Add keycue recipe to sprout-osx-apps

---
+++ sprout-osx-apps/attributes/keycue.rb
@@ -0,0 +1,5 @@
+node.default["sprout"]["keycue"]["package_name"] = "KeyCue"
+node.default["sprout"]["keycue"]["volumes_dir"] = "KeyCue-Install"
+node.default["sprout"]["keycue"]["download_uri"] = "http://www.ergonis.com/downloads/keycue-install.dmg"
+node.default["sprout"]["keycue"]["checksum"] = "1428eaa0d2ab63dfb7a4a83ecabb3b8479617721a6946a23a3a57944e14ca6ff"
+
