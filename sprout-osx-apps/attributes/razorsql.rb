bb6fc76d0c410d29228c0f64e398c7e16bd8b351
Marcos Sainz <marcos+gldr-msainz-desktop@glassdoor.com>
2013-10-18 15:11:31 -0700

    Change RazoerSQL -> RazorSQL in attributes/razorsql.rb

--- sprout-osx-apps/attributes/razorsql.rb
+++ sprout-osx-apps/attributes/razorsql.rb
@@ -1,4 +1,4 @@
-node.default["sprout"]["razorsql"]["package_name"] = "RazoerSQL"
+node.default["sprout"]["razorsql"]["package_name"] = "RazorSQL"
 node.default["sprout"]["razorsql"]["volumes_dir"] = "RazorSQL"
 node.default["sprout"]["razorsql"]["download_uri"] = "http://download1.rstudio.org/RStudio-0.97.551.dmg"
 node.default["sprout"]["razorsql"]["checksum"] = "ec34c96f1427a8d2b6452a0ad53741931b9bc4ec5cf6b61bc294da5c178610cc"
