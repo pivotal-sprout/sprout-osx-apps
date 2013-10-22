1f0676fe2b134677255b86009a0450b7091ddf90
Matt Alexander <mattjalexander@gmail.com>
2013-10-22 15:04:34 -0400

    Update java.rb

--- sprout-osx-apps/attributes/java.rb
+++ sprout-osx-apps/attributes/java.rb
@@ -1,3 +1,3 @@
 default['sprout']['java']['dmg']['source']      = "http://support.apple.com/downloads/DL1572/en_US/JavaForOSX2013-05.dmg"
-default['sprout']['java']['dmg']['volumes_dir'] = "Java for OS X 2013-004"
+default['sprout']['java']['dmg']['volumes_dir'] = "Java for OS X 2013-005"
 default['sprout']['java']['dmg']['checksum']    = "ce78f9a916b91ec408c933bd0bde5973ca8a2dc4"
