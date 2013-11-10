0c9394d368811d60c2f8670585fc273f868116d1
Jason Gladish <jason@expectedbehavior.com>
2013-11-10 18:09:13 -0500

    Added LaunchBar recipe to osx-apps.

---
+++ sprout-osx-apps/attributes/launch_bar.rb
@@ -0,0 +1,3 @@
+default['sprout']['launch_bar']['dmg']['source']      = 'http://www.obdev.at/downloads/launchbar/LaunchBar-5.6.1.dmg'
+default['sprout']['launch_bar']['dmg']['checksum']    = '760111ebdfc480263d7c2ffccedf3de665fa8940e071776dd3b883ae8bbfe39e'
+default['sprout']['launch_bar']['dmg']['volumes_dir'] = 'LaunchBar 5.6.1'
---
+++ sprout-osx-apps/recipes/launch_bar.rb
@@ -0,0 +1,9 @@
+dmg_properties = node['sprout']['launch_bar']['dmg']
+
+dmg_package "LaunchBar" do
+  volumes_dir dmg_properties['volumes_dir']
+  source      dmg_properties['source']
+  checksum    dmg_properties['checksum']
+  action :install
+  owner node['current_user']
+end
