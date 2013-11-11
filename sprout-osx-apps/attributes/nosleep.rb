53bf8dcfc895027bb72c30934fccf685db97aac1
James Cuzella <james.cuzella@lyraphase.com>
2013-11-11 12:25:28 -0700

    Use homebrew cask for nosleep

--- sprout-osx-apps/attributes/nosleep.rb
+++
@@ -1,3 +0,0 @@
-default['sprout']['nosleep']['dmg']['source']      = 'https://macosx-nosleep-extension.googlecode.com/files/NoSleep-1.3.3.dmg'
-default['sprout']['nosleep']['dmg']['checksum']    = '5fe90c54e975d3f1de9f7fdb66def0188c571eab083b18a7e94abaa5f0d41ba9'
-default['sprout']['nosleep']['dmg']['volumes_dir'] = 'NoSleep Extension'
--- sprout-osx-apps/recipes/nosleep.rb
+++ sprout-osx-apps/recipes/nosleep.rb
@@ -1,10 +1,1 @@
-dmg_properties = node['sprout']['nosleep']['dmg']
-
-dmg_package "NoSleep" do
-  volumes_dir dmg_properties['volumes_dir']
-  source      dmg_properties['source']
-  checksum    dmg_properties['checksum']
-  package_id 'com.protech.pkg.NoSleep*' # Optional command line utils: com.protech.pkg.NoSleepCtrl
-  type 'mpkg'
-  owner node['current_user']
-end
+sprout_osx_apps_homebrew_cask "nosleep"
