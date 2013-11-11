d985231fc5e18a7911043d6efbe1b536d8676a7f
James Cuzella <james.cuzella@lyraphase.com>
2013-11-11 12:28:38 -0700

    Use homebrew cask for cyberduck

--- sprout-osx-apps/attributes/cyberduck.rb
+++
@@ -1,5 +0,0 @@
-default['sprout']['cyberduck']['zip']['source'] = 'http://cyberduck.ch/Cyberduck-4.3.1.zip'
-default['sprout']['cyberduck']['zip']['checksum'] = '3fc52a6db95ae761b6bfc91389d0ee51de2b0d161fc308add1c89c16051da2f2'
-default['sprout']['cyberduck']['zip']['app_bundle_name'] = 'Cyberduck.app'
-default['sprout']['cyberduck']['install_dir'] = '/Applications'
-
