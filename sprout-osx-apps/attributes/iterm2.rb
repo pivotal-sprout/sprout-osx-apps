bba4718789f0b2f6de3afceb2cda5f4b4905bb32
Matthew Sykes <matthew.sykes@gmail.com>
2013-11-12 16:33:06 -0800

    Add custom color palettes to iterm plist
    Various custom palettes from github are added to the default plist for
    iterm. 'Solarized Dark' remains the default palette but it can be
    overriden based on sprout properties to something like 'arthur'.

--- sprout-osx-apps/attributes/iterm2.rb
+++ sprout-osx-apps/attributes/iterm2.rb
@@ -2,3 +2,15 @@ default['sprout']['iterm2']['dmg']['source'] = 'https://iterm2.googlecode.com/fi
 default['sprout']['iterm2']['dmg']['checksum'] = '2afad022b1e1f08b3ed40f0c2bde7bf7cce003852c83f85948c7f57a5578d9c5'
 
 default['sprout']['iterm2']['app']['path'] = "/Applications/iTerm.app"
+default['sprout']['iterm2']['color']['custom_urls'] = [
+  'https://raw.github.com/altercation/solarized/master/iterm2-colors-solarized/Solarized%20Dark.itermcolors',
+  'https://raw.github.com/altercation/solarized/master/iterm2-colors-solarized/Solarized%20Light.itermcolors',
+  'https://raw.github.com/baskerville/iTerm-2-Color-Themes/master/arthur.itermcolors',
+  'https://raw.github.com/baskerville/iTerm-2-Color-Themes/master/n0tch2k.itermcolors',
+  'https://raw.github.com/baskerville/iTerm-2-Color-Themes/master/pnevma.itermcolors',
+  'https://raw.github.com/baskerville/iTerm-2-Color-Themes/master/square.itermcolors',
+  'https://raw.github.com/baskerville/iTerm-2-Color-Themes/master/thayer.itermcolors',
+  'https://raw.github.com/baskerville/iTerm-2-Color-Themes/master/wryan.itermcolors',
+  'https://raw.github.com/baskerville/iTerm-2-Color-Themes/master/zenburn.itermcolors'
+]
+default['sprout']['iterm2']['color']['default'] = 'Solarized Dark'
