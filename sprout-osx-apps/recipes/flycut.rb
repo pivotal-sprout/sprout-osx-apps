2563ad19354de0e92c6a1c80d687be142421c656
Brian Cunnie <brian.cunnie@gmail.com>
2013-11-12 19:09:15 -0800

    flycut pulls addloginitem from sprout-osx-base cookbook

--- sprout-osx-apps/recipes/flycut.rb
+++ sprout-osx-apps/recipes/flycut.rb
@@ -1,6 +1,6 @@
 # FIXME: This should be re-factored once package.rb has a mechanism
 # for installing pkg files without mounting a dmg
-include_recipe "pivotal_workstation::addloginitem"
+include_recipe "sprout-osx-base::addloginitem"
 
 app_path="/Applications/Flycut.app"
 
