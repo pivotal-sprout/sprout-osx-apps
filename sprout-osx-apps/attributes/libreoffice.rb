551102423f0de3d5fd2ccaa57f7e87ca3df16cf6
James Cuzella <james.cuzella@lyraphase.com>
2013-11-21 16:44:03 -0700

    Use Homebrew Cask for LibreOffice

--- sprout-osx-apps/attributes/libreoffice.rb
+++
@@ -1,3 +0,0 @@
-default['sprout']['libreoffice']['dmg']['volumes_dir'] = 'LibreOffice'
-default['sprout']['libreoffice']['dmg']['source'] = 'http://download.documentfoundation.org/libreoffice/stable/4.1.2/mac/x86/LibreOffice_4.1.2_MacOS_x86.dmg'
-default['sprout']['libreoffice']['dmg']['checksum'] = 'e6a1dd2e9f49af9b8afd42d014bc38902a527a850058c70eedfe6d3b5ea7bd2a'
