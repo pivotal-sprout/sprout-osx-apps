abc9e50be46504901e464ab650cafddcaf879b1c
James Hart <hjhart@gmail.com>
2013-11-24 23:13:51 -0800

    change default rvm ruby installation to ruby 2 p247

--- pivotal_workstation/attributes/rvm.rb
+++ pivotal_workstation/attributes/rvm.rb
@@ -4,7 +4,7 @@ node.default["rvm"]["rubies"] = {
   # "ruby-1.8.7-p358" => { :env => "CC=gcc-4.2" },
   # "ruby-1.9.2-p290" => { :env => "CC=gcc-4.2" },
   # "ruby-1.9.3-p194" => { :command_line_options => "--with-gcc=clang" }
-  "ruby-1.9.3-p392" => { :command_line_options => "--verify-downloads 1" }
+  "ruby-2.0.0-p247" => { :command_line_options => "--verify-downloads 1" }
 }
 
-node.default["rvm"]["default_ruby"] = "ruby-1.9.3-p392"
+node.default["rvm"]["default_ruby"] = "ruby-2.0.0-p247"
