c6df17444ae2bb12ecc97595c720763abd38a205
Abhi Hiremagalur <abhi@pivotallabs.com>
2013-11-27 18:25:15 +0530

    Update Google Voice and Video dmg again

--- sprout-osx-apps/attributes/google_voice_and_video.rb
+++ sprout-osx-apps/attributes/google_voice_and_video.rb
@@ -1,4 +1,4 @@
 default['sprout']['google_voice_and_video']['dmg']['source'] = 'https://dl.google.com/googletalk/googletalkplugin/GoogleVoiceAndVideoSetup.dmg'
-default['sprout']['google_voice_and_video']['dmg']['checksum'] = '93045db7b81381bfd8861e46f954ae4a78496f32a4b12f75e1dbf58df994a07d'
-default['sprout']['google_voice_and_video']['dmg']['volumes_dir'] = 'GoogleVoiceAndVideoAccelSetup_4.6.3.15268'
+default['sprout']['google_voice_and_video']['dmg']['checksum'] = '5bebc0cfcde9f6e582fb82dacee29a0f8275cabcf7615b2a6f335827e00663e7'
+default['sprout']['google_voice_and_video']['dmg']['volumes_dir'] = 'GoogleVoiceAndVideoAccelSetup_4.9.1.16010'
 default['sprout']['google_voice_and_video']['dmg']['package_id'] = 'com.google.pkg.GoogleVoiceAndVideo'
