675fd6c7bf99e4103aa7bf3a0dbc2daf51cdacc6
Mike Mazur <mmazur@gmail.com>
2013-12-01 20:20:20 -0800

    Parameterize some app install recipes
    Also update hashes and source file URLs.

--- sprout-osx-apps/attributes/chrome.rb
+++ sprout-osx-apps/attributes/chrome.rb
@@ -1,3 +1,3 @@
-default['sprout']['chrome']['dmg']['source']      = 'https://dl.google.com/chrome/mac/stable/CHFA/googlechrome.dmg'
-default['sprout']['chrome']['dmg']['checksum']    = '5cb154403f1e638b9bcca68c0c80dc2b562e9d3a14c7db3e370428dcd451891a'
-default['sprout']['chrome']['dmg']['dmg_name']    = 'googlechrome'
+default['sprout']['chrome']['dmg']['source']   = 'https://dl.google.com/chrome/mac/stable/CHFA/googlechrome.dmg'
+default['sprout']['chrome']['dmg']['checksum'] = 'fa891e9643dfb81d95cf15c7cfa5881ed010518d07dc9ccefa6289c48a24697a'
+default['sprout']['chrome']['dmg']['dmg_name'] = 'googlechrome'
