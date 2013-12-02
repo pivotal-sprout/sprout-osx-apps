f69e3f02832dad60f0d0051e0160338e7999fa0c
Mike Mazur <mmazur@gmail.com>
2013-12-01 20:52:03 -0800

    Parameterize Safari preferences

--- sprout-osx-settings/recipes/safari_preferences.rb
+++ sprout-osx-settings/recipes/safari_preferences.rb
@@ -1,11 +1,11 @@
 osx_defaults "set safari to show status bar" do
   domain 'com.apple.safari'
   key 'ShowStatusBar'
-  boolean true
+  boolean node['safari_preferences']['show_status_bar']
 end
 
 osx_defaults "delete to navigate back" do
   domain 'com.apple.safari'
   key 'com.apple.Safari.ContentPageGroupIdentifier.WebKit2BackspaceKeyNavigationEnabled'
-  boolean true
+  boolean node['safari_preferences']['delete_navigate_back']
 end
