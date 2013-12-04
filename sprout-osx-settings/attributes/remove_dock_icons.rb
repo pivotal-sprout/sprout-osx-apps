222b7f7208fb8ce1a449567726a90c760619a16a
James Hart <hjhart@gmail.com>
2013-12-04 09:46:39 -0800

    Remove dock icons selectively using dockutil

---
+++ sprout-osx-settings/attributes/remove_dock_icons.rb
@@ -0,0 +1,11 @@
+node.default['sprout']['apps_to_remove_from_dock'] = [
+    'Mail',
+    'Launchpad',
+    'Contacts',
+    'Calendar',
+    'Notes',
+    'Maps',
+    'FaceTime',
+    'Photo Booth',
+    'iBooks'
+]
