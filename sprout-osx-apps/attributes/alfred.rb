f029bcfa06b13b9bf6ec1b2d38494c50fa4d1d1c
Mike Mazur <mmazur@gmail.com>
2013-12-05 22:19:26 -0800

    Update alfred recipe
    Alfred is no longer distributed as .dmg but instead as a zip file.

---
+++ sprout-osx-apps/attributes/alfred.rb
@@ -0,0 +1,4 @@
+node.default['sprout']['alfred']['app_name'] = 'Alfred 2.app'
+node.default['sprout']['alfred']['filename'] = 'Alfred_2.1.1_227.zip'
+node.default['sprout']['alfred']['source']   = "http://cachefly.alfredapp.com/#{node['sprout']['alfred']['filename']}"
+node.default['sprout']['alfred']['checksum'] = 'd19fe7441c6741bf663521e561b842f35707b1e83de21ca195aa033cade66d1b'
--- sprout-osx-apps/recipes/alfred.rb
+++ sprout-osx-apps/recipes/alfred.rb
@@ -1,7 +1,27 @@
-dmg_package "Alfred" do
-  volumes_dir "Alfred.app"
-  source "http://cachefly.alfredapp.com/alfred_1.3.1_261.dmg"
-  checksum "c951c4dc05ff1091359358d710142cabef2c190be41f799244669f879cff7e80"
-  action :install
-  owner node['current_user']
+file_properties = node['sprout']['alfred']
+
+unless File.exists?("/Applications/#{file_properties['app_name']}")
+  remote_file "#{Chef::Config[:file_cache_path]}/#{file_properties['filename']}" do
+    source file_properties['source']
+    owner node['current_user']
+    checksum file_properties['checksum']
+  end
+
+  execute "unzip Alfred" do
+    command "unzip #{Chef::Config[:file_cache_path]}/#{file_properties['filename']} -d #{Chef::Config[:file_cache_path]}/"
+    user node['current_user']
+  end
+
+  execute "copy Alfred to /Applications" do
+    command "mv #{Chef::Config[:file_cache_path]}/#{Regexp.escape(file_properties['app_name'])} #{Regexp.escape("/Applications/#{file_properties['app_name']}")}"
+    user node['current_user']
+    group "admin"
+  end
+
+  ruby_block "test to see if 1Password.app was installed" do
+    block do
+      raise "#{file_properties['app_name']} was not installed" unless File.exists?("/Applications/#{file_properties['app_name']}")
+    end
+  end
+
 end
