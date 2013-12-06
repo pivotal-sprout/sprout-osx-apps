9930ad0037c1b3b51c83ee5c25057384be2777fd
Mike Mazur <mmazur@gmail.com>
2013-12-05 22:24:00 -0800

    Parametrize Firefox recipe
    Also update the checksum.

---
+++ sprout-osx-apps/attributes/firefox.rb
@@ -0,0 +1,2 @@
+default['sprout']['firefox']['dmg']['source']   = 'http://download.mozilla.org/?product=firefox-25.0.1&os=osx&lang=en-US'
+default['sprout']['firefox']['dmg']['checksum'] = '09ad0014f72e0f51f1acb88efe6e71fc8f362d6377d160a072f9ccc691905a8d'
