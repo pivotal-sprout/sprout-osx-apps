38253848b9cbd114729024a500a53909f5707dca
Ralph Rooding <ralph@ralphrooding.com>
2013-12-14 00:48:56 +0100

    Make virtualbox recipe use homebrew cask

--- sprout-osx-apps/recipes/virtualbox.rb
+++ sprout-osx-apps/recipes/virtualbox.rb
@@ -1,10 +1,5 @@
-dmg_properties = node['sprout']['virtualbox']['dmg']
+app_path = "/usr/bin/VirtualBox"
 
-dmg_package "VirtualBox" do
-  source dmg_properties['source']
-  checksum dmg_properties['checksum']
-  action :install
-  owner node['current_user']
-  type "pkg"
-  package_id "org.virtualbox.pkg.virtualbox"
+unless File.exists?(app_path)
+  sprout_osx_apps_homebrew_cask "virtualbox"
 end
