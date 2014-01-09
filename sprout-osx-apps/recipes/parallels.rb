01abf75dfbc438b608c1805a67c5ecff791f7c20
Anuj Biyani <abiyani@lytro.com>
2014-01-09 15:51:57 -0800

    use homebrew_cask instead of manually installing it

--- sprout-osx-apps/recipes/parallels.rb
+++ sprout-osx-apps/recipes/parallels.rb
@@ -1,11 +1,1 @@
-dmg_properties = node['sprout']['parallels']['dmg']
-
-dmg_package 'Parallels Desktop' do
-  dmg_name    dmg_properties['dmg_name']
-  source      dmg_properties['source']
-  checksum    dmg_properties['checksum']
-  volumes_dir node['sprout']['parallels']['volumes_dir']
-
-  action :install
-  owner node['current_user']
-end
+sprout_osx_apps_homebrew_cask 'parallels'
