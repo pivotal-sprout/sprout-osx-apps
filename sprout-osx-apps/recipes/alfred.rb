9f7aae2ea20945acd48d22ff47a261cdb6146c7c
Philip Hutchins <flipture@gmail.com>
2014-01-28 10:32:45 -0500

    updating to fix alfred install

--- sprout-osx-apps/recipes/alfred.rb
+++ sprout-osx-apps/recipes/alfred.rb
@@ -1,7 +1,16 @@
-dmg_package "Alfred" do
-  volumes_dir "Alfred.app"
-  source "http://cachefly.alfredapp.com/alfred_1.3.1_261.dmg"
-  checksum "c951c4dc05ff1091359358d710142cabef2c190be41f799244669f879cff7e80"
-  action :install
-  owner node['current_user']
+package_zip = node['sprout']['apps']['alfred']['package_zip']
+package_zip_checksum = node['sprout']['apps']['alfred']['package_zip_checksum']
+package_source = node['sprout']['apps']['alfred']['package_source']
+app_destination = node['sprout']['apps']['alfred']['app_destination']
+downloaded_file = "#{Chef::Config[:file_cache_path]}/#{package_zip}"
+
+remote_file downloaded_file do
+  source File.join(package_source, package_zip)
+  checksum package_zip_checksum
+end
+
+execute "Extract #{package_zip}" do
+  cwd app_destination
+  command %{unzip '#{package_zip}'}
+  creates File.join(app_destination, package_zip)
 end
