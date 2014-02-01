1e33bd069b933a54c00e118dda8930547ef607a2
Ilya Fomin <ifomin@aligntech.com>
2014-02-01 14:44:40 -0800

    Updating sourcetree

--- sprout-osx-apps/recipes/sourcetree.rb
+++ sprout-osx-apps/recipes/sourcetree.rb
@@ -1,7 +1,7 @@
 dmg_package "SourceTree" do
   volumes_dir "SourceTree"
-  source "http://downloads.atlassian.com/software/sourcetree/SourceTree_1.5.7.1.dmg"
-  checksum "cf4f758d3083c6275d7447eacb2d798870d8adddd7a45434e8346bc585ce6d3b"
+  source "http://downloads.atlassian.com/software/sourcetree/SourceTree_1.8.1.dmg"
+  checksum "37a42f2d83940cc7e1fbd573a70c3c74a44134c956ac3305f6b153935dc01b80"
   action :install
   owner node['current_user']
 end
