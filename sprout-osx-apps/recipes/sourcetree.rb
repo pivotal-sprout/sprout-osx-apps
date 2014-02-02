395f6c26d63283b9728de893ee569587a1265cb0
4min <ilya@4min.org>
2014-02-01 19:10:50 -0800

    Parameterized version info for sourcetree

--- sprout-osx-apps/recipes/sourcetree.rb
+++ sprout-osx-apps/recipes/sourcetree.rb
@@ -1,7 +1,9 @@
+dmg_properties = node['sprout']['sourcetree']['dmg']
+
 dmg_package "SourceTree" do
   volumes_dir "SourceTree"
-  source "http://downloads.atlassian.com/software/sourcetree/SourceTree_1.8.1.dmg"
-  checksum "37a42f2d83940cc7e1fbd573a70c3c74a44134c956ac3305f6b153935dc01b80"
+  source dmg_properties['source']
+  checksum dmg_properties['checksum']
   action :install
   owner node['current_user']
 end
