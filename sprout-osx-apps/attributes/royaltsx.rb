6cb1d183b1357e3c4f81af8e29049d0752e6e7ca
4min <ilya@4min.org>
2014-02-01 19:04:44 -0800

    Attributes for most recent RoyalTSX installation

---
+++ sprout-osx-apps/attributes/royaltsx.rb
@@ -0,0 +1,2 @@
+default['sprout']['royaltsx']['dmg']['source'] = 'http://app.royaltsx.com/updates/royaltsx_1.3.0.0.dmg'
+default['sprout']['royaltsx']['dmg']['checksum'] = '2306ae0579b097ccf4796f9cc08012819d3e55d5025a1424c8eca5330cdaedf5'
