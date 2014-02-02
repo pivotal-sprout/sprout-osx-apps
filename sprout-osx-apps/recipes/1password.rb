37997504fa0747b811f2816443e953ccd57f7a4d
Sean Nolen <sean@forgettingtolive.com>
2014-02-02 10:15:42 -0800

    Updating to 1Password 4.1.3 Moving URL and checksum to node attributes Adding force overwrite to unzip command

--- sprout-osx-apps/recipes/1password.rb
+++ sprout-osx-apps/recipes/1password.rb
@@ -1,18 +1,18 @@
 unless File.exists?("/Applications/1Password.app")
 
   remote_file "#{Chef::Config[:file_cache_path]}/1password.zip" do
-    source "https://d13itkw33a7sus.cloudfront.net/dist/1P/mac/1Password-3.8.20.zip"
+    source node['sprout']['1password']['zip']['source']
     owner node['current_user']
-    checksum "56aef138f06fc92d641c424742bc40887cd19e6029f20409ec06ad5514b8cff1"
+    checksum node['sprout']['1password']['zip']['checksum']
   end
 
   execute "unzip 1password" do
-    command "unzip #{Chef::Config[:file_cache_path]}/1password.zip -d #{Chef::Config[:file_cache_path]}/"
+    command "unzip -o #{Chef::Config[:file_cache_path]}/1password.zip -d #{Chef::Config[:file_cache_path]}/"
     user node['current_user']
   end
 
   execute "copy 1password to /Applications" do
-    command "mv #{Chef::Config[:file_cache_path]}/1Password.app #{Regexp.escape("/Applications/1Password.app")}"
+    command "mv #{Regexp.escape("#{Chef::Config[:file_cache_path]}/#{node['sprout']['1password']['zip']['appname']}")} #{Regexp.escape("/Applications/1Password.app")}"
     user node['current_user']
     group "admin"
   end
