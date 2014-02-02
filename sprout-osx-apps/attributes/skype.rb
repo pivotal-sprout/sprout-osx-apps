80f377d568eee41cebc21dcfe57bfbd38630ff97
Mike Mazur <mmazur@gmail.com>
2014-02-01 19:13:12 -0800

    Skype recipe uses Homebrew Cask

--- sprout-osx-apps/attributes/skype.rb
+++
@@ -1,2 +0,0 @@
-default['sprout']['skype']['dmg']['source']   = 'http://download.skype.com/macosx/Skype_6.11.0.455.dmg'
-default['sprout']['skype']['dmg']['checksum'] = '1d3d79524b60246008ded818eb8b0d70'
--- sprout-osx-apps/recipes/skype.rb
+++ sprout-osx-apps/recipes/skype.rb
@@ -1,8 +1,1 @@
-dmg_properties = node['sprout']['skype']['dmg']
-
-dmg_package "Skype" do
-  source   dmg_properties['source']
-  checksum dmg_properties['checksum']
-  owner    node['current_user']
-  action   :install
-end
+sprout_osx_apps_homebrew_cask "skype"
