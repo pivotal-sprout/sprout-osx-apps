d95a4e4727829778ea23ee223d04508f035572b6
Mike Mazur <mmazur@gmail.com>
2014-02-01 18:45:21 -0800

    Dropbox recipe uses Homebrew Cask

--- sprout-osx-apps/attributes/dropbox.rb
+++
@@ -1,2 +0,0 @@
-default['sprout']['dropbox']['dmg']['source'] = 'https://www.dropbox.com/download?plat=mac'
-default['sprout']['dropbox']['dmg']['checksum'] = '662a066f9b1aa3a7facad5b94bd8f82fcd9d4ab0653604d0ddb8e098e9efcd76'
--- sprout-osx-apps/recipes/dropbox.rb
+++ sprout-osx-apps/recipes/dropbox.rb
@@ -1,9 +1,1 @@
-dmg_properties = node['sprout']['dropbox']['dmg']
-
-dmg_package 'Dropbox' do
-  volumes_dir 'Dropbox Installer'
-  source dmg_properties['source']
-  checksum dmg_properties['checksum']
-  action :install
-  owner node['current_user']
-end
+sprout_osx_apps_homebrew_cask "dropbox"
