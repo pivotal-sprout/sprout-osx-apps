0f3f15160ad267bc159ac1d17831a7d3a8675803
Mike Mazur <mmazur@gmail.com>
2014-02-01 19:08:00 -0800

    HipChat recipe uses Homebrew Cask

--- sprout-osx-apps/attributes/hipchat.rb
+++ sprout-osx-apps/attributes/hipchat.rb
@@ -1,4 +1,3 @@
-default['sprout']['hipchat']['url'] = "http://downloads.hipchat.com.s3.amazonaws.com/osx/HipChat-2.4.zip"
 default['sprout']['hipchat']['app'] = "HipChat.app"
 default['sprout']['hipchat']['settings'] = [
   ['disableSounds', 'bool', 'true']
