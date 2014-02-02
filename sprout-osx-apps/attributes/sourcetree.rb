b1daf52743903744cefc831274121a40bbc51951
4min <ilya@4min.org>
2014-02-01 19:07:31 -0800

    Parameters file for sourcetree

---
+++ sprout-osx-apps/attributes/sourcetree.rb
@@ -0,0 +1,2 @@
+default['sprout']['sourcetree']['dmg']['source'] = 'http://downloads.atlassian.com/software/sourcetree/SourceTree_1.8.1.dmg'
+default['sprout']['sourcetree']['dmg']['checksum'] = '37a42f2d83940cc7e1fbd573a70c3c74a44134c956ac3305f6b153935dc01b80'
