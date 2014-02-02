4bdf9073272035a08ecc2026f869925514d1bba7
Mike Mazur <mmazur@gmail.com>
2014-02-01 18:23:27 -0800

    VLC recipe uses Homebrew Cask

--- sprout-osx-apps/attributes/vlc.rb
+++
@@ -1,2 +0,0 @@
-node.default["vlc_version"]  = "2.1.1"
-node.default["vlc_checksum"] = "d41ee3ae5f67479680f202de594e8ea1c58dbf2010b7963534d1f707e15c9707"
--- sprout-osx-apps/recipes/vlc.rb
+++ sprout-osx-apps/recipes/vlc.rb
@@ -1,11 +1,1 @@
-vlc_version = node["vlc_version"]
-vlc_checksum = node["vlc_checksum"]
-
-dmg_package "VLC" do
-  dmg_name "vlc-#{vlc_version}"
-  volumes_dir "vlc-#{vlc_version}"
-  source "http://download.videolan.org/vlc/#{vlc_version}/macosx/vlc-#{vlc_version}.dmg"
-  checksum vlc_checksum
-  action :install
-  owner node['current_user']
-end
+sprout_osx_apps_homebrew_cask "vlc"
