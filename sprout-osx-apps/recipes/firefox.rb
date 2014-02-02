bca8dacbefd78683f0cf7ec7c547748d31587435
Mike Mazur <mmazur@gmail.com>
2014-02-01 18:57:07 -0800

    Firefox recipe uses Homebrew Cask

--- sprout-osx-apps/recipes/firefox.rb
+++ sprout-osx-apps/recipes/firefox.rb
@@ -1,8 +1,1 @@
-dmg_properties = node['sprout']['firefox']['dmg']
-
-dmg_package "Firefox" do
-  source   dmg_properties['source']
-  checksum dmg_properties['checksum']
-  action :install
-  owner node['current_user']
-end
+sprout_osx_apps_homebrew_cask "firefox"
