afbfe45c3fb30b4b2ec743d4e3958bb4d775287b
Mike Mazur <mmazur@gmail.com>
2014-02-01 18:39:30 -0800

    Google Chrome uses Homebrew Cask

--- sprout-osx-apps/attributes/chrome.rb
+++
@@ -1,3 +0,0 @@
-default['sprout']['chrome']['dmg']['source']   = 'https://dl.google.com/chrome/mac/stable/CHFA/googlechrome.dmg'
-default['sprout']['chrome']['dmg']['checksum'] = 'fa891e9643dfb81d95cf15c7cfa5881ed010518d07dc9ccefa6289c48a24697a'
-default['sprout']['chrome']['dmg']['dmg_name'] = 'googlechrome'
