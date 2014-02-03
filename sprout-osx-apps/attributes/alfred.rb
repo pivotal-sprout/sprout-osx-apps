3c4a4bdfd5a0e941aa0edd4d836833d893c8452e
Philip Hutchins <flipture@gmail.com>
2014-02-03 08:12:50 -0500

    fixing typo in attributes

--- sprout-osx-apps/attributes/alfred.rb
+++ sprout-osx-apps/attributes/alfred.rb
@@ -1,4 +1,4 @@
 default['sprout']['apps']['alfred']['package_zip'] = 'Alfred_2.1.1_227.zip'
-defualt['sprout']['apps']['alfred']['package_source'] = 'http://cachefly.alfredapp.com/'
+default['sprout']['apps']['alfred']['package_source'] = 'http://cachefly.alfredapp.com/'
 default['sprout']['apps']['alfred']['package_zip_checksum'] = 'db28d2a5c655a4611c780c3f46252530118ddd9d'
 default['sprout']['apps']['alfred']['app_destination'] = '/Applications'
