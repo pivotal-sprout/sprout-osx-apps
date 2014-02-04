1b680e37b2e047dc19eb887faaaad69f1f4b5200
Ilya Fomin <ifomin@aligntech.com>
2014-02-04 10:56:11 -0800

    Syntax fix for office installation

--- sprout-osx-apps/recipes/msoffice.rb
+++ sprout-osx-apps/recipes/msoffice.rb
@@ -7,12 +7,12 @@ remote_file "#{Chef::Config[:file_cache_path]}/office2011.sh" do
   source 'https://raw.github.com/tjluoma/office2011/master/office2011.sh'
   owner node['current_user']
   mode '700'
-  not_if { File.not_exists?('/Applications/Microsoft Office 2011' }
+  not_if { File.not_exists?('/Applications/Microsoft Office 2011') }
 end
 
 execute "install msoffice" do
   command "#{Chef::Config[:file_cache_path]}/office2011.sh"
   cwd Chef::Config[:file_cache_path]
   user node['current_user']
-  not_if { File.not_exists?('/Applications/Microsoft Office 2011' }
+  not_if { File.not_exists?('/Applications/Microsoft Office 2011') }
 end
