c9a5d6e5526705ed04f6828a521ab9395111d41b
Abhi Hiremagalur <abhi@pivotallabs.com>
2014-02-04 18:21:25 -0800

    Switch to homebrew cask for java
    Old recipe was broken on Mavericks

--- sprout-osx-apps/attributes/java.rb
+++
@@ -1,3 +0,0 @@
-default['sprout']['java']['dmg']['source']      = "http://support.apple.com/downloads/DL1572/en_US/JavaForOSX2013-05.dmg"
-default['sprout']['java']['dmg']['volumes_dir'] = "Java for OS X 2013-005"
-default['sprout']['java']['dmg']['checksum']    = "81e1155e44b2c606db78487ca1a02e31dbb3cfbf7e0581a4de3ded9e635a704e"
