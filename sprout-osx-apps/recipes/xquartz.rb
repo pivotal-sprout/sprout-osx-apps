7bc7c70d7b32dc950a2d980961e0a7ba7adbdc73
Abhi Hiremagalur <abhi@pivotallabs.com>
2014-02-04 18:16:10 -0800

    Switch to homebrew cask for xquartz
    Old recipe was broken on Mavericks

--- sprout-osx-apps/recipes/xquartz.rb
+++ sprout-osx-apps/recipes/xquartz.rb
@@ -1,9 +1,1 @@
-dmg_package "XQuartz" do
-  source "http://xquartz.macosforge.org/downloads/SL/XQuartz-2.7.4.dmg"
-  action :install
-  volumes_dir "XQuartz-2.7.4"
-  checksum "3f7c156fc4b13e3f0d0e44523ef2bd3cf7ea736126616dd2da28abb31840923c"
-  type "pkg"
-  owner node['current_user']
-  package_id "org.macosforge.xquartz.pkg"
-end
+sprout_osx_apps_homebrew_cask 'xquartz'
