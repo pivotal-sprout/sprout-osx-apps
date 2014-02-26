41d262fd3b9406f524033043c8144fdc748e5cbf
Philip Hutchins <flipture@gmail.com>
2014-02-25 19:01:24 -0500

    fixing sourceforge url to avoid chef redirect limit

--- sprout-osx-apps/attributes/adium.rb
+++ sprout-osx-apps/attributes/adium.rb
@@ -1,3 +1,3 @@
-default['sprout']['adium']['dmg']['source']      = 'http://sourceforge.net/projects/adium/files/Adium_1.5.7.dmg/download'
+default['sprout']['adium']['dmg']['source']      = 'http://downloads.sourceforge.net/projects/adium/Adium_1.5.7.dmg/download'
 default['sprout']['adium']['dmg']['checksum']    = 'f80408a2c5bc97be8adb35ffeb7c4501392e847edae4e6e24d87da72de7e96b9'
 default['sprout']['adium']['dmg']['volumes_dir'] = 'Adium 1.5.7'
