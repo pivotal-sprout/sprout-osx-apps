9fa4a94770bfa3a6e43647e3e1638735694e61a1
Abhi Hiremagalur <abhijit@hiremaga.com>
2014-06-11 11:36:54 -0700

    Convert handbrake to homebrew_cask

--- attributes/handbrake.rb
+++
@@ -1,4 +0,0 @@
-default['sprout']['handbrake']['dmg']['source']      = 'https://downloads.sourceforge.net/project/handbrake/0.9.9/HandBrake-0.9.9-MacOSX.6_GUI_x86_64.dmg?r=http%3A%2F%2Fhandbrake.fr%2F&ts=1374994193&use_mirror=iweb'
-default['sprout']['handbrake']['dmg']['checksum']    = '050f9d0d3a126b25d24cb4971062d66f4f975317b6586f8c288795c17a0c05f9'
-default['sprout']['handbrake']['dmg']['volumes_dir'] = 'HandBrake-0.9.9-MacOSX.6_GUI_x86_64'
-default['sprout']['handbrake']['dmg']['type']        = 'app'
