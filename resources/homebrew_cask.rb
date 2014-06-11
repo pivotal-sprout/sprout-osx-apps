03336644b757cf4d49648cdc89e6525736640d92
Abhi Hiremagalur <abhijit@hiremaga.com>
2014-06-11 12:05:50 -0700

    No need for cask LWRP since homebrew cookbook has one

--- resources/homebrew_cask.rb
+++
@@ -1,6 +0,0 @@
-actions :install
-
-default_action :install
-
-attribute :name, :kind_of => String, :name_attribute => true
-attribute :opts, :kind_of => String, :default => '--appdir="/Applications"'
