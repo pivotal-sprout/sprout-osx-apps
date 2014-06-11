1866857d169295ae82b88c2df6c27feec0a31b40
Abhi Hiremagalur <abhijit@hiremaga.com>
2014-06-11 11:26:21 -0700

    Convert clamxav to homebrew_cask

--- recipes/clamxav.rb
+++ recipes/clamxav.rb
@@ -1,8 +1,1 @@
-dmg_package 'ClamXav' do
-  dmg_properties = node['sprout']['clamxav']['dmg']
-  action :install
-  checksum dmg_properties['checksum']
-  owner node['current_user']
-  source dmg_properties['source']
-  volumes_dir dmg_properties['volumes_dir']
-end
+homebrew_cask 'clamxav'
