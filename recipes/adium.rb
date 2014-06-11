263b242e9e9bbb21a57ec9b99987f4a308980529
Abhi Hiremagalur <abhijit@hiremaga.com>
2014-06-11 11:24:00 -0700

    Convert adium to homebrew_cask

--- recipes/adium.rb
+++ recipes/adium.rb
@@ -1,9 +1,1 @@
-dmg_properties = node['sprout']['adium']['dmg']
-
-dmg_package "Adium" do
-  volumes_dir dmg_properties['volumes_dir']
-  source      dmg_properties['source']
-  checksum    dmg_properties['checksum']
-  action :install
-  owner node['current_user']
-end
+homebrew_cask 'adium'
