58743492138bb940a0c27f429660bf31d44ec8cf
Abhi Hiremagalur <abhijit@hiremaga.com>
2014-06-11 11:39:21 -0700

    Convert intellij to homebrew_cask

--- recipes/intellij_ultimate_edition.rb
+++ recipes/intellij_ultimate_edition.rb
@@ -1,6 +1,1 @@
-dmg_package node["intellij_ultimate_edition_package_name"] do
-  source node["intellij_ultimate_edition_download_uri"]
-  checksum node["intellij_ultimate_edition_sha"]
-  owner node['current_user']
-  action :install
-end
+homebrew_cask 'intellij-idea'
