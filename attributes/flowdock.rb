9f312fa579b8ef2e98f7a3d075f4df39570ffcab
Abhi Hiremagalur <abhijit@hiremaga.com>
2014-06-11 11:32:56 -0700

    Convert flowdock to homebrew_cask

--- attributes/flowdock.rb
+++
@@ -1,3 +0,0 @@
-node.default["flowdock_download_uri"]="https://d2ph5hv9wbwvla.cloudfront.net/mac/Flowdock_v1_1_0.zip"
-node.default["flowdock_sha"]="c8a2529630740313daae772ac884463c6271f46c5b2d0555e0340594051dedd8"
-node.default["flowdock_app_path"]="/Applications/Flowdock.app"
