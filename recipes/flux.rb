8317f31e415c036b70e527e0b57a5e5aeadd3f49
Abhi Hiremagalur <abhijit@hiremaga.com>
2014-06-11 11:33:31 -0700

    Convert flux to homebrew_cask

--- recipes/flux.rb
+++ recipes/flux.rb
@@ -1,12 +1,1 @@
-include_recipe "sprout-base::addloginitem"
-
-app_path ="/Applications/Flux.app"
-
-unless File.exists?(app_path)
-  homebrew_cask "f-lux"
-
-  execute "Start Flux on login" do
-    command "addloginitem #{app_path}"
-    user node['current_user']
-  end
-end
+homebrew_cask 'flux'
