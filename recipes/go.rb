c46a88f75021b14d2ee9398c8a2261eae28d69e5
Matthew Sykes <matthew.sykes@gmail.com>
2014-08-12 11:08:57 -0700

    Add godoc to sprout-osx-apps::go
    Signed-off-by: Abhijit Hiremagalur <abhi@pivotallabs.com>

--- recipes/go.rb
+++ recipes/go.rb
@@ -9,3 +9,7 @@ end
 sprout_base_bash_it_custom_plugin 'bash_it/custom/go.bash' do
   cookbook 'sprout-osx-apps'
 end
+
+execute '/usr/local/bin/go get code.google.com/p/go.tools/cmd/godoc' do
+  environment('GOPATH' => File.join(node['sprout']['home'], 'go'))
+end
--- templates/default/bash_it/custom/go.bash
+++ templates/default/bash_it/custom/go.bash
@@ -1,2 +1,2 @@
 export GOPATH=$HOME/go
-export PATH=$PATH:$GOPATH/bin
+export PATH=$PATH:$GOPATH/bin:/usr/local/opt/go/libexec/bin
