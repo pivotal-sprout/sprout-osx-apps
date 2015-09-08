45428b6e21547e5045939651447b8db7750dc964
Dan Wendorf <dwendorf@pivotal.io>
2015-09-07 18:27:21 -0700

    Use current cask repo instead of phinze
    - Update README.md
    - Update homebrew_cask recipe

--- recipes/homebrew_cask.rb
+++ recipes/homebrew_cask.rb
@@ -1,7 +1,7 @@
 include_recipe "sprout-base::homebrew"
 
-execute "tap phinze/homebrew-cask" do
-  command "brew tap phinze/homebrew-cask"
+execute "tap caskroom/homebrew-cask" do
+  command "brew tap caskroom/homebrew-cask"
   not_if { system("brew tap | grep 'cask' > /dev/null 2>&1") }
 end
 
