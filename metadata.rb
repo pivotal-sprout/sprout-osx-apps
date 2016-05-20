0dad0d9d494353e1d2ee2e62d64846ce1933891b
Brian Cunnie <bcunnie@pivotal.io>
2016-05-20 11:24:47 -0700

    Bump version in anticipation of removing addLoginItem functionality
    Signed-off-by: aram price <aprice@pivotal.io>

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'sprout-maintainers@googlegroups.com'
 license          'MIT'
 description      'Installs/Configures sprout-osx-apps'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.2.0'
 supports         'mac_os_x'
 depends          'sprout-base'
 depends          'sprout-osx-settings'
