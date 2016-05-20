38899c86b2e841deb60fe5f9d3f294e73549f204
aram price <aprice@pivotal.io>
2016-05-20 11:25:06 -0700

    Stop relying on addLoginItem since it will be removed
    Signed-off-by: Brian Cunnie <bcunnie@pivotal.io>

--- recipes/shiftit.rb
+++ recipes/shiftit.rb
@@ -1,8 +1,4 @@
-include_recipe 'sprout-base::addloginitem'
-
-app_path='/Applications/ShiftIt.app'
-
-unless File.exists?(app_path)
+unless File.exists?('/Applications/ShiftIt.app')
   remote_file "#{Chef::Config[:file_cache_path]}/ShiftIt.zip" do
     source 'https://raw.github.com/onsi/ShiftIt/master/ShiftIt.zip'
     mode '0644'
@@ -12,11 +8,6 @@ unless File.exists?(app_path)
     command "unzip #{Chef::Config[:file_cache_path]}/ShiftIt.zip ShiftIt.app/* -d /Applications/"
     user node['sprout']['user']
     group 'admin'
-  end
-
-  # start up on login
-  execute('Start ShiftIt on login') do
-    command "su #{node['sprout']['user']} -c \"addloginitem #{app_path}\""
   end
 
   ruby_block 'Allow ShiftIt to control your computer' do
